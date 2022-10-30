79e07069d35e5df02877d2b834cb6d35712821d6
BuGGames2 <abbosbek.otabaev@mail.ru>
2022-10-30 14:07:25 +0300

    Groveamde

---
+++ script.js
@@ -0,0 +1,39 @@
+$('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0}, 800); // 800 - Скорость анимации
+});
+
+$(window).scroll(function() { // Отслеживаем начало прокрутки
+    let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+
+    if(scrolled > 350) { // Если высота прокрутки больше 350 - показываем кнопку
+        $('.back-to-top').addClass('active');
+    } else {
+        $('.back-to-top').removeClass('active');
+    }
+});
+
+const btn = document.querySelector('.footer__btn')
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function doSomething(scrollPos) {
+    if(scrollPos >= 710){
+        btn.classList.add('active')
+    }else{
+        btn.classList.remove('active')
+    }
+}
+
+document.addEventListener('scroll', (e) => {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
